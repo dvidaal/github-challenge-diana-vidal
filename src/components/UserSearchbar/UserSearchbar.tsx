ee100ff436ee72fb0abc4b60634e120cee5a1203
Diana Vidal <dvidaal@gmail.com>
2023-11-24 12:51:12 +0100

    Create user searchbar to finde a user

---
+++ src/components/UserSearchbar/UserSearchbar.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Button from "../Button/Button";
+import getUser from "../../hooks/getUser/getUser";
+import { UserData } from "../../types/types";
+
+const UserSearchbar = (): JSX.Element => {
+  const [user, setUser] = useState<UserData>({
+    login: "",
+    avatar_url: "",
+    repos_url: "",
+  });
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSearch = async () => {
+    const response = await getUser(inputValue);
+    setUser(response);
+  };
+
+  const onChangeHandler = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(value);
+  };
+
+  return (
+    <>
+      <h1>GitHub Glorified List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Type GitHub username"
+          value={inputValue}
+          onChange={onChangeHandler}
+        />
+        <Button action={handleSearch} />
+        <span>{user.login}</span>
+      </div>
+    </>
+  );
+};
+
+export default UserSearchbar;
