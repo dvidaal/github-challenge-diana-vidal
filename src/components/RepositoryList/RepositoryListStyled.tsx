8cfe9fc385a316bd1d1d8d849ac7310c8d5e308b
Diana Vidal <dvidaal@gmail.com>
2023-11-28 17:43:13 +0100

    Create new component to separate repositorylist from userprofile

---
+++ src/components/RepositoryList/RepositoryListStyled.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const RepositoryListStyled = styled.div`
+  width: 100%;
+  .repository-list {
+    &__repository-section {
+      display: flex;
+      flex-direction: column;
+      list-style: none;
+      padding: 0;
+      width: 100%;
+    }
+
+    &__repositories {
+      color: #000;
+      font-family: "Roboto";
+      font-weight: 900;
+      font-size: 1.5rem;
+      border-bottom: 1px solid #ccc;
+      padding: 10px;
+    }
+
+    &__description {
+      font-style: italic;
+      font-size: 1rem;
+    }
+
+    &__language {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      font-size: 1rem;
+      font-weight: 100;
+    }
+
+    &__repositories:last-child {
+      border-bottom: none;
+    }
+  }
+`;
+
+export default RepositoryListStyled;
